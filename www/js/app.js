6a932b22dfb55812944e515098fa17f7f9353c45
Tom Scanlan <tscanlan@mchs.org>
2016-09-25 16:06:06 -0500

    first

---
+++ www/js/app.js
@@ -0,0 +1,75 @@
+(function(){
+var app = angular.module('mynotes', ['ionic', 'mynotes.notestore'])
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+  $stateProvider.state('edit', {
+    url: '/edit/:noteId',
+    templateUrl: 'templates/edit.html',
+    controller: 'EditCtrl'
+  });
+  $stateProvider.state('add', {
+    url: '/add',
+    templateUrl: 'templates/edit.html',
+    controller: 'AddCtrl'
+  });
+  $urlRouterProvider.otherwise('/list');
+});
+
+app.controller('ListCtrl', function($scope, NoteStore){
+  $scope.notes = NoteStore.list()
+  $scope.reordering = false;
+
+  $scope.remove = function(noteId){
+    NoteStore.remove(noteId)
+  }
+  $scope.move = function(note, fromIndex, toIndex){
+    NoteStore.move(note, fromIndex, toIndex)
+  }
+  $scope.toggleReordering = function(){
+    $scope.reordering = !$scope.reordering
+  }
+})
+
+app.controller('AddCtrl', function($scope, $state, NoteStore){
+
+  $scope.note = {
+    title: "",
+    description: "",
+    id: new Date().getTime().toString()
+  }
+  
+  $scope.save = function(){
+    NoteStore.create($scope.note);
+    $state.go('list')
+  }
+
+})
+
+app.controller('EditCtrl', function($scope, $state, NoteStore){
+
+  $scope.note = angular.copy(NoteStore.get($state.params.noteId));
+
+  $scope.save = function(){
+    NoteStore.update($scope.note);
+    $state.go('list')
+  }
+
+})
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+}());
---
+++ www/js/notestore.js
@@ -0,0 +1,55 @@
+angular.module('mynotes.notestore', [])
+
+.factory('NoteStore', function(){
+  var notes = angular.fromJson(window.localStorage['notes'] || '[]');
+
+  function persist(){
+    window.localStorage['notes'] = angular.toJson(notes);
+  }
+
+  return {
+    list: function(){
+      return notes
+    },
+    get: function(noteId){
+      for (var i=0; i<notes.length; i++){
+        if(notes[i].id === noteId){
+          return notes[i]
+        }
+      }
+      return undefined
+
+    },
+    create: function(note){
+       notes.push(note);
+       persist();
+    },
+    update: function(note){
+      for (var i=0; i<notes.length; i++){
+        if(notes[i].id === note.id){
+          notes[i] = note;
+          persist();
+          return;
+        }
+      }
+      return undefined
+      },
+      move: function(note, fromIndex, toIndex){
+        notes.splice(fromIndex,1);
+
+        notes.splice(toIndex,0,note);
+        persist();
+      },
+    remove: function(noteId){
+      for (var i=0; i<notes.length; i++){
+        if(notes[i].id === noteId){
+          notes.splice(i, 1);
+          persist();
+          return;
+        }
+      }
+    }
+
+   }
+
+})
